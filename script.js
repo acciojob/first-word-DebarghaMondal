4b0f46da6a77ed732ac5549f279954b9aa8d16fd
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-14 11:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function firstWord(s) {
-  // your code here  
-    if(!s){
-		return '';
-	}
-	const initialWord = s.split(' ');
-	let firstWord = initialWord[0];
-	return firstWord;
+// your code here
+if (s === ‘’) {
+return ‘’;
 }
+
+let firstWord = s.split(" ")[0]
+return firstWord;
+} 
+
+
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt(“Enter String:”);
 alert(firstWord(s));
